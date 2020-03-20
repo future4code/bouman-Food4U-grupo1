f73146a147034d41ce4e9660186396ef7fc45e82
MateusGesualdo <mateusgcs99@gmail.com>
2020-03-20 12:36:38 -0300

    Primeiro desafio

--- src/business/usecases/users/SignupUC.ts
+++ src/business/usecases/users/SignupUC.ts
@@ -1,11 +1,13 @@
 import UserDB from "../../../data/UserDatabase";
 import User from "../../entities/User";
-import {v4} from "uuid"; 
+import { v4 } from "uuid";
 import * as bcrypt from 'bcrypt';
 
 interface SignupInput {
+    name: string
     email: string
     password: string
+    birthDate: string
 }
 
 export default class SignupUC {
@@ -14,9 +16,17 @@ export default class SignupUC {
     ) { }
 
     async execute(input: SignupInput) {
+        
         const id = v4()
         const rounds = 10
-        const hash = await bcrypt.hash(input.password, rounds)
-        await this.database.signup(new User(id, input.email, hash))
+        const hashPassword = await bcrypt.hash(input.password, rounds)
+
+        await this.database.signup(new User(
+            id,            
+            input.email,
+            hashPassword,
+            input.name,
+            input.birthDate
+        ))
     }
 }
--- src/data/UserDatabase.ts
+++ src/data/UserDatabase.ts
@@ -2,16 +2,25 @@ import MainDB from './MainDatabase';
 import User from '../business/entities/User';
 
 export default class UserDB extends MainDB {
-    async signup(user: User){
-        await this.connection.raw(`INSERT into USERS values("${user.getId()}", "${user.getEmail()}", "${user.getPassword()}")`)
+    async signup(user: User) {
+        await this.connection.raw(`INSERT into USERS values(
+            "${user.getId()}",             
+            "${user.getEmail()}", 
+            "${user.getPassword()}",
+            "${user.getName()}",
+            "${user.getBirthDate()}"
+        )`)
     }
 
-    async getUserByEmail(email: string){
-        const query = await this.connection.raw(`SELECT id, password from USERS WHERE email = "${email}"`)
+    async getUserByEmail(email: string) {
+        const query = await this.connection.raw(
+            `SELECT id, email, name, birth_date 
+            FROM USERS WHERE email = "${email}"`
+            )
         return query[0][0]
     }
 
-    async follow(followerId:string, userToFollowId:string){
+    async follow(followerId: string, userToFollowId: string) {
         await this.connection.raw(
             `INSERT INTO FOLLOWERS VALUES (
                 "${followerId}",
